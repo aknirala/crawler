fcd323c511279099a420fe5d2923a84a4a9e1820
Ashutosh Nirala <akn.nirala@gmail.com>
2015-12-13 23:53:13 +0530

    Word Vector Count after reading json (v basic)

--- CreateWordVector.py
+++ CreateWordVector.py
@@ -1,8 +1,18 @@
 import string
+import json
 from collections import Counter
+
+
 
 def remove_punctuations(word):
     return word.translate(None, string.punctuation)
 
-with open("ToVectorize.txt") as f:
-    print Counter(map(remove_punctuations, f.readlines()[0].split() ))["significant"]
+with open("20151116.json") as json_file:
+    json_data = json.load(json_file)
+    s = ''
+    for d in json_data:
+        s += d['content']
+    print(Counter(  s.split()  ))
+    # Why is the below line giving error?
+    # print(Counter( map(remove_punctuations,  s.split())  ))
+    # pprint(Counter(map(remove_punctuations, json_file.readlines()[0].split() )))
